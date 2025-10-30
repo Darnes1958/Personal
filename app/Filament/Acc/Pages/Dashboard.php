603bb3decaa474234a305f1d12c3fbca44bba777
unknown <bdel_ati_m@yahoo.com>
2025-10-30 10:28:13 +0200

    print

--- app/Filament/Acc/Pages/Dashboard.php
+++ app/Filament/Acc/Pages/Dashboard.php
@@ -2,7 +2,7 @@
 
 namespace App\Filament\Acc\Pages;
 
-class AccDashboard extends \Filament\Pages\Dashboard
+class Dashboard extends \Filament\Pages\Dashboard
 {
     protected ?string $heading="منظومة المحاسبة - الصفحة الرئيسية";
     public function getColumns(): int|array
